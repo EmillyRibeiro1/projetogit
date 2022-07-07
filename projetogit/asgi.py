6ceab0eacd08cbb7cdcf3a9984bcc23ed6a34fca
Emilly <emillyribeirosouza123@hotmail.com>
2022-07-07 09:29:00 -0300

    primeiro commit

---
+++ projetogit/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for projetogit project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.0/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'projetogit.settings')
+
+application = get_asgi_application()
